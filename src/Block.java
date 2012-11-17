b3a409f7ab32de286ec094abcd81be5e6fcafb56
Brian Edmonds <brian@brian-VirtualBox.(none)>
2012-11-17 09:23:01 -0500

    Initial Commit

---
+++ src/Block.java
@@ -0,0 +1,12 @@
+
+public class Block {
+	boolean dirty;
+	boolean valid;
+	int offset;
+	int tag;
+	int index;
+	public Block(){
+		
+		
+	}
+}
---
+++ src/Cache.java
@@ -0,0 +1,15 @@
+
+public class Cache {
+	public Cache(){
+		
+	}
+	void access(char rw, String address){
+		
+	}
+	public static void main(String[]args){
+		System.out.println("Hello World");
+	}
+}
+//put main here
+
+
